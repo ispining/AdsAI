2cfdefb639e2bf9d7fe312f327684ff4f8aebc4f
sbdtAdmin <sbdt.israel@gmail.com>
2024-11-11 23:09:38 +0200

    ~ /src/texts.py created

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from pprint import pprint
 from src import connections as conn
 
-pprint(conn.my_chats(types=["supergroup"]))
+selected_group = conn.my_chats(types=["supergroup"])[0]
+
+iter_num = 0
+for m_info in conn.get_chat_messages(selected_group["id"]):
+    print(m_info)
+    print()
+    iter_num += 1
+    if iter_num == 5:
+        break
 
--- src/connections.py
+++ src/connections.py
@@ -1,23 +1,24 @@
 from pyrogram import Client
 api_id = 21194050 
 api_hash = "ef8c398ae656ae51e585e405d3e83ff2"                     
-sender_app = Client("Main", api_id, api_hash)                     
-sender_app.start()                                             
+tg = Client("Main", api_id, api_hash)
+tg.start()
+
 
 def get_chat_messages(chat_id):                                       
-    with sender_app as bot:                                               
-        return bot.get_chat_history(chat_id)                  
-  
+    return tg.get_chat_history(chat_id)
+
+
 def send_message(chat_id, message, photo=None):                     
-    with sender_app as bot:                                      
-        if photo:                                         
-            bot.send_photo(chat_id, photo, caption=message)          
-        else:                                                   
-            bot.send_message(chat_id, message)                             
+    if photo:
+        tg.send_photo(chat_id, photo, caption=message)
+    else:
+        tg.send_message(chat_id, message)
+
 
 def my_chats(types=["private", "group", "supergroup"]):       
     result = []                                                
-    for dialog in sender_app.get_dialogs():                        
+    for dialog in tg.get_dialogs():
         if dialog.chat.type.value in types:                                 
             result.append({                                     
                 "id": dialog.chat.id,                   
