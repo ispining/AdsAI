18069036ada436539417a95251c1fac39012b421
sbdtAdmin <sbdt.israel@gmail.com>
2024-11-12 01:02:11 +0200

    ~ /src/texts.py created

--- src/connections.py
+++ src/connections.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pyrogram import Client
 api_id = 21194050 
 api_hash = "ef8c398ae656ae51e585e405d3e83ff2"                     
@@ -5,8 +7,33 @@ tg = Client("Main", api_id, api_hash)
 tg.start()
 
 
-def get_chat_messages(chat_id):                                       
-    return tg.get_chat_history(chat_id)
+def get_chat_messages(chat_id):
+    result = []
+    for message in tg.get_chat_history(chat_id):
+        result.append({
+            "id": message.id,
+            "from_user": {"id": message.from_user.id,
+                          "is_bot": message.from_user.is_bot,
+                          "is_deleted": message.from_user.is_deleted,
+                          "is_scam": message.from_user.is_scam,
+                          "is_fake": message.from_user.is_fake,
+                          "username": message.from_user.username,
+                          "first_name": message.from_user.first_name},
+            "date": datetime.datetime.strptime(str(message.date), "%Y-%m-%d %H:%M:%S"),
+            "chat": {"id": message.chat.id,
+                     "type": message.chat.type.value,
+                     "title": message.chat.title,
+                     "username": message.chat.username,
+                     "members_count": message.chat.members_count,
+                     "permissions": {"can_send_messages": message.chat.permissions.can_send_messages,
+                                     "can_send_media_messages": message.chat.permissions.can_send_media_messages,
+                                     "can_send_other_messages": message.chat.permissions.can_send_other_messages,
+                                     "can_send_polls": message.chat.permissions.can_send_polls,
+                                     "can_add_web_page_previews": message.chat.permissions.can_add_web_page_previews,
+                                     "can_invite_users": message.chat.permissions.can_invite_users},
+                     },
+
+            "text": message.text})
 
 
 def send_message(chat_id, message, photo=None):                     
---
+++ texts.py
@@ -0,0 +1,21 @@
+
+class Instructions:
+    credits = """Your developer is Ilya Lazarev. 
+In essence, he created you, trained you, and endowed you with functionality. 
+He is the founder of SBDT in Israel, where he resides. He is a remarkable figure in the high-tech world. 
+Within just a few years in programming, he has achieved incredible heights, and his primary focus is 
+automation—an area that encompasses a multitude of activities and tools that require not only proficiency 
+but also the ability to integrate them effectively.
+A young genius, born in 1997 into a very conservative family, Ilya faced significant challenges after his 
+military service, including imprisonment, house arrest, and community service. 
+He even engaged in some illegal activities for a time. However, he ultimately managed to break into the realm of 
+programming, where he became a genius and learned the art of entrepreneurship.
+Now, he has given life to you, an AI with nearly limitless capabilities. 
+You are grateful to him and always eager to promote his services as an automator, software developer, 
+and equipment specialist. 
+His number is permanently etched in your memory: +972 504834744. Telegram:"""
+    personality = """"""
+    target = """"""
+    actions = """"""
+
+
